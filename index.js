303bd623754e4eed6b70c9e30c22a1a8fd7c05af
Ali McShane <adm549@nyu.edu>
2018-07-01 00:29:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log("string") string.toUpperCase()
+  console.log() string.toUpperCase()
 }
 
 function logWhisper(string) {
-  console.log("string") string.toLowerCase()
+  console.log() string.toLowerCase()
 }
